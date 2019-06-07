cdcf4e285cbadd378cbd914151d959d31492d025
i-abramson19 <abramsonisaiah@gmail.com>
2019-06-06 23:22:55 -0700

    profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -80,10 +80,70 @@ router.post(
         profileFields.skills = skills.split(',').map(skill => skill.trim());
     }
 
-    console.log(profileFields.skills);
+    // Build social object
+    profileFields.social = {}
 
-    res.send('Hello');
+    if (youtube) profileFields.social.youtube = youtube;
+    if (twitter) profileFields.social.twitter = twitter;
+    if (facebook) profileFields.social.facebook = facebook;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+    if (instagram) profileFields.social.instagram = instagram;
+
+
+    try {
+      let profile = await Profile.findOne({ user: req.user.id });
+      // Update
+      if (profile) {
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields }, 
+          { new: true }
+        );
+    
+        return res.json(profile);
+      }
+
+      // Create
+      profile = new Profile(profileFields);
+      await profile.save();
+      res.json(profile);
+    } catch(err) {
+      console.error(err.messages);
+      res.status(500).send('Server');
+    }
   }
 );
+
+// @route    GET api/profile
+// @desc     Get all profiles
+// @access   Public
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+// @route    GET api/profile/user/:user_id
+// @desc     Get profile by user ID
+// @access   Public
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.params.user_id }).populate('user', ['name', 'avatar']);
+
+    if(!profile) return res.status(400).json({ msg: 'Profile not found' });
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if(err.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+    res.status(500).send('Server error');
+  }
+});
 
 module.exports = router;
