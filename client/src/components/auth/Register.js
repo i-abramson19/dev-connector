6987af46960f2a272244b26c595f0fd24ea3afbd
i-abramson19 <abramsonisaiah@gmail.com>
2019-06-10 00:49:53 -0700

    alerts

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,8 +1,10 @@
 import React, { Fragment, useState } from 'react';
-import axios from 'axios';
+import { connect } from 'react-redux'
 import { Link } from 'react-router-dom';
+import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
 
-const Register = () => {
+const Register = ({ setAlert }) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -17,31 +19,9 @@ const Register = () => {
     const onSubmit = async e => {
         e.preventDefault();
         if(password !== password2) {
-            console.log('Passwords do not match');
+            setAlert('Passwords do not match', 'danger');
         } else {
-            /*
-            const newUser = {
-                name,
-                email,
-                password
-            }
-
-            try {
-                const config = {
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                }
-
-                const body = JSON.stringify(newUser);
-
-                const res = await axios.post('/api/users', body, config);
-
-                console.log(res.data);
-            } catch (err) {
-                console.error(err.response.data);
-            }
-            */
+            console.log('Success');
         }
     }
 
@@ -71,8 +51,7 @@ const Register = () => {
                     />
                     <small className="form-text"
                         >This site uses Gravatar so if you want a profile image, use a
-                        Gravatar email</small
-                    >
+                        Gravatar email</small>
                 </div>
                 <div className="form-group">
                     <input
@@ -105,4 +84,8 @@ const Register = () => {
     );
 };
 
-export default Register;
+Register.propTypes = {
+    setAlert: PropTypes.func.isRequired
+};
+
+export default connect(null, { setAlert })(Register);
