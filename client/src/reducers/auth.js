47bfb01134752504f5ba206a9cd84bdb536cbf7e
i-abramson19 <abramsonisaiah@gmail.com>
2019-06-10 16:54:37 -0700

    auth

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,6 +1,11 @@
 import {
     REGISTER_SUCCESS,
-    REGISTER_FAIL
+    REGISTER_FAIL,
+    USER_LOADED,
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    LOGOUT
 } from '../actions/types';
 
 const initialState = {
@@ -17,7 +22,15 @@ export default function(state = initialState, action) {
     } = action;
 
     switch(type) {
+        case USER_LOADED:
+            return {
+                ...state,
+                isAuthenticated: true,
+                loading: false,
+                user: payload
+            };
         case REGISTER_SUCCESS:
+        case LOGIN_SUCCESS:
             localStorage.setItem('token', payload.token);
             return {
                 ...state,
@@ -26,6 +39,9 @@ export default function(state = initialState, action) {
                 loading: false
             };
         case REGISTER_FAIL:
+        case AUTH_ERROR:
+        case LOGIN_FAIL:
+        case LOGOUT:
             localStorage.removeItem('token');
             return {
                 ...state,
