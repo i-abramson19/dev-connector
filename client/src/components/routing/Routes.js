52dae4b7034a8ad9a1721b799b3c0652b6c1be1d
i-abramson19 <abramsonisaiah@gmail.com>
2019-06-19 20:22:37 -0700

    profile

--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -4,7 +4,8 @@ import Register from '../auth/Register';
 import Login from '../auth/Login';
 import Alert from '../layout/Alert';
 import Dashboard from '../dashboard/Dashboard';
-import CreateProfile from '../profile-form/CreateProfile';
+import CreateProfile from '../profile-forms/CreateProfile';
+import EditProfile from '../profile-forms/EditProfile';
 import PrivateRoute from '../routing/PrivateRoute';
 
 const Routes = () => {
@@ -16,6 +17,7 @@ const Routes = () => {
         <Route exact path='/login' component={Login} />
         <PrivateRoute exact path='/dashboard' component={Dashboard} />
         <PrivateRoute exact path='/create-profile' component={CreateProfile} />
+        <PrivateRoute exact path='/edit-profile' component={EditProfile} />
       </Switch>
     </section>
   );
