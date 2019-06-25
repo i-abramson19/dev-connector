eace698be51b7bef7fdfff7c8687c5b8ca638c49
i-abramson19 <abramsonisaiah@gmail.com>
2019-06-24 17:15:12 -0700

    profile

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -14,11 +14,6 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
         <Link to='/posts'>Posts</Link>
       </li>
       <li>
-        <Link to='/profiles'>
-          Developers
-        </Link>
-      </li>
-      <li>
         <Link to='/dashboard'>
           <i className='fas fa-user' />{' '}
           <span className='hide-sm'>Dashboard</span>
