a2e580388d5fd0b6bf8385923fdd604c33c0d238
i-abramson19 <abramsonisaiah@gmail.com>
2019-06-24 20:14:42 -0700

    posts

--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -10,6 +10,7 @@ import AddExperience from '../profile-forms/AddExperience';
 import AddEducation from '../profile-forms/AddEducation';
 import Profiles from '../profiles/Profiles';
 import Profile from '../profile/Profile';
+import Posts from '../posts/Posts';
 import PrivateRoute from '../routing/PrivateRoute';
 
 const Routes = () => {
@@ -26,6 +27,7 @@ const Routes = () => {
         <PrivateRoute exact path='/edit-profile' component={EditProfile} />
         <PrivateRoute exact path='/add-experience' component={AddExperience} />
         <PrivateRoute exact path='/add-education' component={AddEducation} />
+        <PrivateRoute exact path='/posts' component={Posts} />
       </Switch>
     </section>
   );
